7eb95d29cfd372199a5fd3fa81bdf34208eb7ab2
Woutsthew <nekita351@gmail.com>
2022-03-28 11:45:26 +0400

    проверка на наличие программы в датафайлах, если нет то создать копию

--- IntegrityVerification/MyDataFile.cs
+++ IntegrityVerification/MyDataFile.cs
@@ -14,7 +14,8 @@ namespace IntegrityVerification
 
             if (File.Exists(Constants.dataFilePath) == false)
             {
-                File.Copy(Process.GetCurrentProcess().MainModule.FileName, Constants.appFilePath);
+                if (File.Exists(Constants.appFilePath) == false)
+                    File.Copy(Process.GetCurrentProcess().MainModule.FileName, Constants.appFilePath);
 
                 File.Create(Constants.dataFilePath).Close();
                 File.WriteAllText(Constants.dataFilePath, JsonConvert.SerializeObject(new List<TrackedObject>(), Formatting.Indented));
