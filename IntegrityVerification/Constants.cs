07b7bda613d8cbc3cf6a9952b7c15af37b276f9a
Woutsthew <nekita351@gmail.com>
2022-03-28 11:26:10 +0400

    константы с расположение установленного приложения

--- IntegrityVerification/Constants.cs
+++ IntegrityVerification/Constants.cs
@@ -27,10 +27,12 @@ namespace IntegrityVerification
         public const string r_COMMAND = "command";
 
 
+        private static string appFileName = Path.GetFileName(Process.GetCurrentProcess().MainModule.FileName);
         private const string dataFileName = "TrackedObjects.txt";
 
         private static readonly string roaming = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
         public static readonly string appDirectoryPath = Path.Combine(Constants.roaming, Process.GetCurrentProcess().ProcessName);
+        public static readonly string appFilePath = Path.Combine(Constants.appDirectoryPath, appFileName);
         public static readonly string dataFilePath = Path.Combine(Constants.appDirectoryPath, Constants.dataFileName);
     }
 }
--- IntegrityVerification/MyDataFile.cs
+++ IntegrityVerification/MyDataFile.cs
@@ -14,10 +14,10 @@ namespace IntegrityVerification
 
             if (File.Exists(Constants.dataFilePath) == false)
             {
+                File.Copy(Process.GetCurrentProcess().MainModule.FileName, Constants.appFilePath);
+
                 File.Create(Constants.dataFilePath).Close();
                 File.WriteAllText(Constants.dataFilePath, JsonConvert.SerializeObject(new List<TrackedObject>(), Formatting.Indented));
-                File.Copy(Process.GetCurrentProcess().MainModule.FileName,
-                    Path.Combine(Constants.appDirectoryPath, Path.GetFileName(Process.GetCurrentProcess().MainModule.FileName)));
             }
 
             return true;
