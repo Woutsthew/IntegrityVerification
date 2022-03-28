348711ae2f96f637aad46d1622ad630e39808c9a
Woutsthew <nekita351@gmail.com>
2022-03-28 11:20:17 +0400

    создает копию файла в директории с дата файлом

--- IntegrityVerification/MyDataFile.cs
+++ IntegrityVerification/MyDataFile.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 
 
@@ -15,6 +16,8 @@ namespace IntegrityVerification
             {
                 File.Create(Constants.dataFilePath).Close();
                 File.WriteAllText(Constants.dataFilePath, JsonConvert.SerializeObject(new List<TrackedObject>(), Formatting.Indented));
+                File.Copy(Process.GetCurrentProcess().MainModule.FileName,
+                    Path.Combine(Constants.appDirectoryPath, Path.GetFileName(Process.GetCurrentProcess().MainModule.FileName)));
             }
 
             return true;
